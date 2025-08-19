e6b9b3b2ca4b9d8e18974a3a75821e9c43270358
Alex <adityalasika@gmail.com>
2025-08-20 04:07:58 +0700

    Update to modern Python 3.8+ with latest dependencies
    - Updated setup.py to support Python 3.8-3.12
    - Added pyproject.toml for modern Python packaging
    - Created requirements.txt with updated dependencies
    - Updated .gitignore for Python development
    - Tested all functionality works with Python 3.9

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
   #packages = ['vaderSentiment'], # this must be the same as the name above
   packages = find_packages(exclude=['tests*']), # a better way to do it than the line above -- this way no typo/transpo errors
   include_package_data=True,
-  version = '3.3.1',
+  version = '3.3.2',
   description = 'VADER Sentiment Analysis. VADER (Valence Aware Dictionary and sEntiment Reasoner) is a lexicon and rule-based sentiment analysis tool that is specifically attuned to sentiments expressed in social media, and works well on texts from other domains.',
   long_description = read("README.rst"),
   long_description_content_type = 'text/x-rst',
@@ -25,13 +25,23 @@ setup(
   license = 'MIT License: http://opensource.org/licenses/MIT',
   url = 'https://github.com/cjhutto/vaderSentiment', # use the URL to the github repo
   download_url = 'https://github.com/cjhutto/vaderSentiment/archive/master.zip',
-  install_requires = ['requests'],
+  install_requires = ['requests>=2.31.0'],
+  python_requires = '>=3.8',
   keywords = ['vader', 'sentiment', 'analysis', 'opinion', 'mining', 'nlp', 'text', 'data',
               'text analysis', 'opinion analysis', 'sentiment analysis', 'text mining', 'twitter sentiment',
               'opinion mining', 'social media', 'twitter', 'social', 'media'], # arbitrary keywords
-  classifiers = ['Development Status :: 4 - Beta', 'Intended Audience :: Science/Research',
-                 'License :: OSI Approved :: MIT License', 'Natural Language :: English',
-                 'Programming Language :: Python :: 3.5', 'Topic :: Scientific/Engineering :: Artificial Intelligence',
-                 'Topic :: Scientific/Engineering :: Information Analysis', 'Topic :: Text Processing :: Linguistic',
+  classifiers = ['Development Status :: 5 - Production/Stable', 
+                 'Intended Audience :: Science/Research',
+                 'License :: OSI Approved :: MIT License', 
+                 'Natural Language :: English',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10',
+                 'Programming Language :: Python :: 3.11',
+                 'Programming Language :: Python :: 3.12',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                 'Topic :: Scientific/Engineering :: Information Analysis', 
+                 'Topic :: Text Processing :: Linguistic',
                  'Topic :: Text Processing :: General'],
 )
